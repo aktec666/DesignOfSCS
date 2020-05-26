7661d6eaf3663700b7305275bead812303536852
aktec <aktec@mail.ru>
2020-05-26 23:03:28 +0300

    Add comments kruskal algorithm

--- DesignOfSCS/math/KruskalAlgorithm.cs
+++ DesignOfSCS/math/KruskalAlgorithm.cs
@@ -10,30 +10,30 @@ namespace DesignOfSCS.math
 		/// <summary>
 		/// Структура ребро
 		/// </summary>
-		public struct Edge
+		public struct Edge // структура ребро
 		{
-			public int Source;
-			public int Destination;
-			public double Weight;
+			public int Source; // вершина 1
+			public int Destination; // вершина 2
+			public double Weight; // вес между вершинами 1 и 2
 		}
 
 		/// <summary>
 		/// Структура граф
 		/// </summary>
-		public struct Graph
+		public struct Graph // структура граф
 		{
-			public int VerticesCount;
-			public int EdgesCount;
-			public Edge[] edge;
+			public int VerticesCount; // количество вершин
+			public int EdgesCount; // количество ребер
+			public Edge[] edge; // список ребер из структуры Edge
 		}
 
 		/// <summary>
 		/// Компонента связности
 		/// </summary>
-		public struct Subset
+		public struct Subset // структура компонента связности
 		{
-			public int Parent;
-			public int Rank;
+			public int Parent; // ссылка на родителя
+			public int Rank; // ранг - глубина дерева
 		}
 
 		/// <summary>
@@ -44,12 +44,12 @@ namespace DesignOfSCS.math
 		/// <returns></returns>
 		public static Graph CreateGraph(int verticesCount, int edgesCoun)
 		{
-			Graph graph = new Graph();
-			graph.VerticesCount = verticesCount;
-			graph.EdgesCount = edgesCoun;
-			graph.edge = new Edge[graph.EdgesCount];
+			Graph graph = new Graph(); // конструтор графа
+			graph.VerticesCount = verticesCount; // записываем количество вершин
+			graph.EdgesCount = edgesCoun; // записываем количетсво ребер
+			graph.edge = new Edge[graph.EdgesCount]; // инициализируем список из ребер
 
-			return graph;
+			return graph; // возращаем полученный граф
 		}
 
 		/// <summary>
@@ -58,9 +58,9 @@ namespace DesignOfSCS.math
 		/// <param name="subsets">список множеств</param>
 		/// <param name="i">номер компоненты</param>
 		/// <returns></returns>
-		private static int Find(Subset[] subsets, int i)
+		private static int Find(Subset[] subsets, int i) // рекурсивная функция по поиску конечного родителя
 		{
-			if (subsets[i].Parent != i)
+			if (subsets[i].Parent != i) // база рекурсии
 				subsets[i].Parent = Find(subsets, subsets[i].Parent);
 
 			return subsets[i].Parent;
@@ -72,19 +72,19 @@ namespace DesignOfSCS.math
 		/// <param name="subsets">список множеств</param>
 		/// <param name="x">Компонента 1</param>
 		/// <param name="y">Компонента 2</param>
-		private static void Union(Subset[] subsets, int x, int y)
+		private static void Union(Subset[] subsets, int x, int y) // объединяем 2 компоненты x и y
 		{
-			int xroot = Find(subsets, x);
+			int xroot = Find(subsets, x); // находим корни деревьев
 			int yroot = Find(subsets, y);
 
-			if (subsets[xroot].Rank < subsets[yroot].Rank)
+			if (subsets[xroot].Rank < subsets[yroot].Rank) // используем эвристику проверки на ранг, выбираем с меньшим рангом
 				subsets[xroot].Parent = yroot;
 			else if (subsets[xroot].Rank > subsets[yroot].Rank)
 				subsets[yroot].Parent = xroot;
 			else
 			{
-				subsets[yroot].Parent = xroot;
-				++subsets[xroot].Rank;
+				subsets[yroot].Parent = xroot; // создаем ребро
+				++subsets[xroot].Rank; // увеличиваем ранг
 			}
 		}
 
@@ -93,26 +93,26 @@ namespace DesignOfSCS.math
 		/// </summary>
 		/// <param name="_graph">граф</param>
 		/// <returns></returns>
-		public static int[,] Kruskal(DesignOfSCS.graph.Graph _graph)
+		public static int[,] Kruskal(DesignOfSCS.graph.Graph _graph) // на вход подается граф DesignOfSCS.graph.Graph
 		{
 
 			// конвертируем граф в удобный формат для алгоритма Краскала
-			Graph graph = KruskalAlgorithm.CreateGraph(_graph.Nodes.Count, _graph.Edges.Count);
-			for (int j = 0; j < graph.EdgesCount; j++)
+			Graph graph = KruskalAlgorithm.CreateGraph(_graph.Nodes.Count, _graph.Edges.Count); // передаем параметры в конструтор 
+			for (int j = 0; j < graph.EdgesCount; j++)// Заполняем массив ребер
 			{
-				graph.edge[j].Source = _graph.Edges[j].From.Id;
-				graph.edge[j].Destination = _graph.Edges[j].To.Id;
-				graph.edge[j].Weight = _graph.Edges[j].Weight;
+				graph.edge[j].Source = _graph.Edges[j].From.Id; // записываем вершину 1
+				graph.edge[j].Destination = _graph.Edges[j].To.Id; // записываем вершину 2
+				graph.edge[j].Weight = _graph.Edges[j].Weight; // записываем вес между вершинами 1 и 2
 			}
 
 
-			int verticesCount = graph.VerticesCount;
-			Edge[] result = new Edge[verticesCount];
-			int i = 0;
-			int e = 0;
+			int verticesCount = graph.VerticesCount; // записываем количество вершин
+			Edge[] result = new Edge[verticesCount]; // создаем массив result - в нем будет хранится минимальный остов
+			int i = 0; // счетчик ребер
+			int e = 0; // счетчик добавленных ребер
 
 			// сортируем список ребер по возрастанию
-			Array.Sort(graph.edge, delegate (Edge a, Edge b)
+			Array.Sort(graph.edge, delegate (Edge a, Edge b) // вызываем встроенную сортировку и сортируем по возрастанию ребра на основании веса
 			{
 				return a.Weight.CompareTo(b.Weight);
 			});
@@ -120,26 +120,26 @@ namespace DesignOfSCS.math
 			// множество компонент связсности
 			Subset[] subsets = new Subset[verticesCount];
 
-			for (int v = 0; v < verticesCount; ++v)
+			for (int v = 0; v < verticesCount; ++v) // заполняем компоненты связности, изначально их столько же сколько и вершин, у нас n деревьев
 			{
-				subsets[v].Parent = v;
-				subsets[v].Rank = 0;
+				subsets[v].Parent = v; // изначально ссылка на самого себя, ребер пока нет
+				subsets[v].Rank = 0; // глубина дерева изначально 0
 			}
 
-			while (e < verticesCount - 1)
+			while (e < verticesCount - 1) // перебираем все ребра
 			{
-				Edge nextEdge = graph.edge[i++];
-				int x = Find(subsets, nextEdge.Source);
+				Edge nextEdge = graph.edge[i++]; // достаем ребро со списка
+				int x = Find(subsets, nextEdge.Source); // находим компоненты
 				int y = Find(subsets, nextEdge.Destination);
 
-				if (x != y)
+				if (x != y) // если они не связаны, объединяем
 				{
 					result[e++] = nextEdge;
 					Union(subsets, x, y);
 				}
 			}
 
-			int[,] p = new int[e,2];
+			int[,] p = new int[e,2]; // формируем ответ
 
 			for(int j = 0; j < e; j++)
 			{
